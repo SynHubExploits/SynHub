f3dd8f60e96307d619e696fe2884d20ca048d73a
SynHubExploits <58152790@leerling.raayland.nl>
2025-06-18 14:23:03 +0200

    Update DashBoard.lua

--- DashBoard.lua
+++ DashBoard.lua
@@ -1,1 +1,11 @@
+local PGui = game.Player.LocalPlayer.PlayerGui
 
+local Container = instance.new("ScreenGui")
+Container.IgnoreGuilnset = true
+Container.Parent = PGui
+
+local ActivateButton = instance.new("ImageButton")
+ActivateButton..Parent = Container
+ActivateButton.Size = Udim2.New(0.05,0,0.1,0)
+ActivateButton.Position = Udim2.New(0.1,0,0.5,0)
+ActivateButton.AnchorPoint = Vector2.New(0.5,0.5)
