2fd08017e84f0c38fc6584cc9a1e9c458c7b730e
SynHubExploits <58152790@leerling.raayland.nl>
2025-06-18 14:18:47 +0200

    Update and rename Dashboard.lua to Load.lua

--- Load.lua
+++ Load.lua
@@ -3,10 +3,10 @@ local PlayerGui = player.PlayerGui
 game:GetService("StarterGui"):SetCore("SendNotification", {
     Title = "SynHub",
     Text = "Loading!",
-    Duration = 5
+    Duration = 2
 })
 
-Wait(1)
+Wait(2)
 
 game:GetService("StarterGui"):SetCore("SendNotification", {
     Title = "SynHub,
